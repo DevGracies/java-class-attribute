ea702b8018c2981c2556dac36d666417d9d05834
Grace <105996232+DevGracies@users.noreply.github.com>
2023-05-30 21:07:08 +0100

    Create Myclass.java

---
+++ Myclass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    //attribute
+    String name = "Grace", career = "Software Developer";
+    public static void main(String args[]) {
+      MyClass newObj = new MyClass();
+      System.out.println("Hello I'm " + newObj.name + ". I'm a " + newObj.career);
+      
+      
+    }
+}
